5f5e3729d7f78ee092c79d91a4a91b10b36b2fd0
Prateek <it.1803283@gmail.com>
2026-08-07 02:28:43 +0530

    Allow frontend CORS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const API = 'http://127.0.0.1:8000'
+const API = "https://crud-app-backend-5kn0.onrender.com"
 
 function App() {
   const [users, setUsers] = useState([])
